8e91a37cfb9257a4d671b1c2734e87b70e1c9a27
Raju Yadav <yadavxraju@gmail.com>
2025-02-26 15:11:41 +0530

    Update Index.js
    make folder

--- Frontend/Index.js
+++ Frontend/Index.js
@@ -2,3 +2,6 @@ console.log(rishabh);
 console.log(hello);
 console.log(Hello2)
 console.log("hellow rworld")
+console.log("hellow rworld")
+console.log("hellow rworld")
+console.log("hellow rworld")
